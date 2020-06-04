6328dbe61cb36678e818a72f52dfb8de635e4df7
yclept <huber.robert542@gmail.com>
2020-06-04 15:37:08 -0700

    working table

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import React from "react";
 import "./styles.css";
-import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css';
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       columnDefs: [
         { headerName: "Domain", field: "domain" },
         { headerName: "Scope", field: "scope" },
-        { headerName: "App", field: "app" }],
+        { headerName: "App", field: "app" }
+      ],
       rowData: [
-        { domain: "ifm", scope: "ifm:read", app: "app1"},
-        { domain: "OVM", scope: "ovm:create_order", app: "app2"},
-        { domain: "ACE", scope: "ACE:read", app: "app3"}]
-    }
+        { domain: "ifm", scope: "ifm:read", app: "app1" },
+        { domain: "OVM", scope: "ovm:create_order", app: "app2" },
+        { domain: "ACE", scope: "ACE:read", app: "app3" }
+      ]
+    };
   }
 
   render() {
     return (
-      <div className="ag-theme-alpine" style={ {height: '200px', width: '600px'} }>
+      <div
+        className="ag-theme-alpine"
+        style={{ height: "200px", width: "600px" }}
+      >
         <AgGridReact
-            columnDefs={this.state.columnDefs}
-            rowData={this.state.rowData}>
-        </AgGridReact>
+          columnDefs={this.state.columnDefs}
+          rowData={this.state.rowData}
+        />
       </div>
     );
   }
 }
 
-render(<App />, document.getElementById('root'));
+export default App;
