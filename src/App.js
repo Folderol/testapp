057535eef2688706f73926fab0ae081ac5589e17
yclept <huber.robert542@gmail.com>
2020-06-07 19:02:59 -0700

    adjusted data.

--- src/App.js
+++ src/App.js
@@ -8,24 +8,28 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      columnDefs: [
-          { headerName: "Id", field: "id", sortable: true, filter: true },
-          { headerName: "Name", field: "name", sortable: true, filter: true },
-          { headerName: "Description", field: "description", sortable: true, filter: true },
-          { headerName: "Domain Name", field: "domain", sortable: true, filter: true }
-      ],
-      rowData: [
-        {id:"A1",name:"apiPerimissionRead",description:"apiPerimissionRead",domain:"abc"},
-        {id:"A2",name:"apiPerimissionUpdate",description:"apiPerimissionUpdate",domain:"abc"},
-        {id:"A3",name:"apiPreferenceRead",description:"apiPreferenceRead",domain:"abc"},
-        {id:"A4",name:"apiPreferenceUpdate",description:"apiPreferenceUpdate",domain:"def"},
-        {id:"A5",name:"apiNameDefRead",description:"apiNameDefRead",domain:"def"},
-        {id:"A6",name:"apiNameDefUpdate",description:"apiNameDefUpdate",domain:"def"},
-        {id:"A7",name:"validateApiCallRead",description:"validateApiCallRead",domain:"ghi"},
-        {id:"A8",name:"validateApiCallUpdate",description:"validateApiCallUpdate",domain:"ghi"},
-        {id:"A9",name:"apiAcccountInfoRead",description:"apiAcccountInfoRead",domain:"jkl"},
-        {id:"A10",name:"apiAccountUpdate",description:"apiAccountUpdate",domain:"jkl"}
-      ]
+      api_data = {
+        columnDefs: [
+            { headerName: "Id", field: "id", sortable: true, filter: true },
+            { headerName: "Name", field: "name", sortable: true, filter: true },
+            { headerName: "Description", field: "description", sortable: true, filter: true },
+            { headerName: "Domain Name", field: "domain", sortable: true, filter: true }
+        ],
+        rowData: [
+          {id:"A1",name:"apiPerimissionRead",description:"apiPerimissionRead",domain:"abc"},
+          {id:"A2",name:"apiPerimissionUpdate",description:"apiPerimissionUpdate",domain:"abc"},
+          {id:"A3",name:"apiPreferenceRead",description:"apiPreferenceRead",domain:"abc"},
+          {id:"A4",name:"apiPreferenceUpdate",description:"apiPreferenceUpdate",domain:"def"},
+          {id:"A5",name:"apiNameDefRead",description:"apiNameDefRead",domain:"def"},
+          {id:"A6",name:"apiNameDefUpdate",description:"apiNameDefUpdate",domain:"def"},
+          {id:"A7",name:"validateApiCallRead",description:"validateApiCallRead",domain:"ghi"},
+          {id:"A8",name:"validateApiCallUpdate",description:"validateApiCallUpdate",domain:"ghi"},
+          {id:"A9",name:"apiAcccountInfoRead",description:"apiAcccountInfoRead",domain:"jkl"},
+          {id:"A10",name:"apiAccountUpdate",description:"apiAccountUpdate",domain:"jkl"}
+        ]
+      },
+      columnDefs: apiData.columnDefs,
+      rowData: apiData.rowData
     };
   }
 
