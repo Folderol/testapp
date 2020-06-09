16cb80b80b240e275062f21230a2eca7c89d18dc
yclept <huber.robert542@gmail.com>
2020-06-08 17:33:56 -0700

    fix nav collapse

--- src/components/Navigator.js
+++ src/components/Navigator.js
@@ -14,12 +14,12 @@ class Navigator extends React.Component {
     return (
             <Navbar bg="dark" expand="lg">
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav"/>
-
+                <Navbar.Collapse id="basic-navbar-nav">
                 <Nav>
                 <NavLink className="d-inline p-2 bg-dark text-white" to="/">Api</NavLink>
                 <NavLink className="d-inline p-2 bg-dark text-white" to="/scope">Scope</NavLink>
                 </Nav>
+                </Navbar.Collapse>
             </Navbar>
         )
     }
