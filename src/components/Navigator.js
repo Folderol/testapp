2bcf9472d34a2e108b6c0bc9b6e022a82ea39d74
yclept <huber.robert542@gmail.com>
2020-06-09 16:11:47 -0700

    keep Navigator

---
+++ src/components/Navigator.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+
+class Navigator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      somedata: ""
+    };
+  }
+
+  render() {
+    return (
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/">
+              Api
+            </NavLink>
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/scope">
+              Scope
+            </NavLink>
+            <NavLink
+              className="d-inline p-2 bg-dark text-white"
+              to="/clientscope"
+            >
+              Client Scope
+            </NavLink>
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/domain">
+              Domain
+            </NavLink>
+            <NavLink className="d-inline p-2 bg-dark text-white" to="/client">
+              Client
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default Navigator;
