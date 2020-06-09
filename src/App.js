010d8a003fd45068e1fbcb68081bdf65432d6eb8
yclept <huber.robert542@gmail.com>
2020-06-09 16:13:19 -0700

    bugfix

--- src/App.js
+++ src/App.js
@@ -178,7 +178,7 @@ class App extends React.Component {
           <Switch>
             <Route path="/" component={(routeProps) => <Gentab title="Api" tableDef={this.state.apiData} {...routeProps} /> } exact />
             <Route path="/api" component={(routeProps) => <Gentab title="Api" tableDef={this.state.apiData} {...routeProps} /> } />
-            <Route path="/scope" component={(routeProps) => <Scope title="Scope" tableDef={this.state.apiData} {...routeProps} /> } />
+            <Route path="/scope" component={(routeProps) => <Scope title="Scope" tableDef={this.state.scopeData} {...routeProps} /> } />
             <Route path="/clientscope" component={(routeProps) => <Gentab title="Client Scope" tableDef={this.state.clientScopeData} rowData={this.state.scopeRowData} {...routeProps} /> } />
             <Route path="/domain" component={(routeProps) => <Gentab title="Domain" tableDef={this.state.domainData} {...routeProps} /> } />
             <Route path="/client" component={(routeProps) => <Gentab title="Client" tableDef={this.state.clientData} {...routeProps} /> } />
